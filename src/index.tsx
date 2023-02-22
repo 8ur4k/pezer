4a128b49a6a65e0132fc2abf762f0a95f9fa1432
Burak Ilgenci <burakonduty@MacBook-Pro-2.local>
2023-02-22 13:32:00 +0300

    Add Sentry to capture errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+Sentry.init({
+  dsn: "https://28a54869157e4ca9a7a878e3f9491015@o4504722959499264.ingest.sentry.io/4504722967691264",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
